4e84dede91d941fa8abb5db27dfaecc6354dbdee
feeroza <160626229+feeroza@users.noreply.github.com>
2024-08-08 21:58:49 +0500

    Add files via upload

---
+++ Question 8/008.js
@@ -0,0 +1,13 @@
+// 8. You should create four lines that look like this:
+//_____________________________________________
+//console.log(5 + 3)
+//_____________________________________________
+//Your output should simply be four lines with the number 8 appearing once on each line.
+//additio.
+console.log(2 + 6);
+//substraction
+console.log(12 - 4);
+//multiply
+console.log(4 * 2);
+//division
+console.log(48 / 6);
